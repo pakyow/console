8694334a94dab01faf0de0205dc8a0d5ed8e10c3
Bryan Powell <bryan@metabahn.com>
2015-07-15 17:48:17 -0500

    Redactor for text editor

--- lib/editors/text_editor.rb
+++ lib/editors/text_editor.rb
@@ -1,3 +1,3 @@
 Pakyow::Console::EditorRegistry.register :text do
-  Pakyow::Presenter::View.new('<textarea data-scope="editor" style="resize:none" cols="80" rows="8"></textarea>')
+  Pakyow::Presenter::View.new('<textarea data-scope="editor" style="resize:none" cols="80" rows="8" data-ui="redactor"></textarea>')
 end
