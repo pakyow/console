183728cf990fc89c637d2c4b77c483cff765d49f
Bryan Powell <bryan@metabahn.com>
2015-11-05 21:19:56 -0600

    Move websocketclient / release agent away from celluloid

--- lib/app/misc/release_agent.rb
+++ lib/app/misc/release_agent.rb
@@ -1,144 +1,141 @@
-# TODO: refactor away from celluloid
-# require 'securerandom'
+require 'securerandom'
+require 'concurrent'
 
-# require 'celluloid'
-# require 'celluloid/io'
+class ReleaseAgent
+  DEPLOY_PATH = File.expand_path('./')
 
-# class ReleaseAgent
-#   include Celluloid::IO
-#   finalizer :shutdown
+  ADDONS = [
+    'heroku-postgresql:hobby-dev',
+    'heroku-redis:hobby-dev',
+  ]
 
-#   DEPLOY_PATH = File.expand_path('./')
-#   ADDONS = [
-#     'heroku-postgresql:hobby-dev',
-#     'heroku-redis:hobby-dev',
-#   ]
+  attr_reader :heroku_client, :platform_client, :release_object
 
-#   attr_reader :heroku_client, :platform_client, :release_object
+  def initialize(heroku_client, platform_client, release_object)
+    @heroku_client = heroku_client
+    @platform_client = platform_client
+    @release_object = release_object
 
-#   def initialize(heroku_client, platform_client, release_object)
-#     @heroku_client = heroku_client
-#     @platform_client = platform_client
-#     @release_object = release_object
+    @id = SecureRandom.hex(10)
+    perform
+  end
 
-#     @id = SecureRandom.hex(10)
-#     async.perform
-#   end
+  def perform
+    Concurrent::Future.execute do
+      log 'performing a new release'
+      setup
+      release
+    end
+  end
 
-#   def perform
-#     log 'performing a new release'
-#     setup
-#     release
-#   end
+  def setup
+    return if setup?
 
-#   def setup
-#     return if setup?
+    log 'getting things set up'
 
-#     log 'getting things set up'
+    app = heroku_client.app.create({})
+    path = File.expand_path('./.platform')
+    config = Hash.strhash(JSON.parse(File.open(path).read))
+    config[:release] = { app: app }
+    f = File.open(path, 'w')
+    f.write(config.to_json)
+    f.close
 
-#     app = heroku_client.app.create({})
-#     path = File.expand_path('./.platform')
-#     config = Hash.strhash(JSON.parse(File.open(path).read))
-#     config[:release] = { app: app }
-#     f = File.open(path, 'w')
-#     f.write(config.to_json)
-#     f.close
+    ADDONS.each do |name|
+      log "provisioning addon for #{app['id']}: #{name}"
+      heroku_client.addon.create(app['id'], {
+        plan: name
+      })
+    end
 
-#     ADDONS.each do |name|
-#       log "provisioning addon for #{app['id']}: #{name}"
-#       heroku_client.addon.create(app['id'], {
-#         plan: name
-#       })
-#     end
+    log 'setup complete'
+  end
 
-#     log 'setup complete'
-#   end
+  def release
+    log 'doing the release'
+    platform_client.update_release(release_object[:id], { status: :pending })
 
-#   def release
-#     log 'doing the release'
-#     platform_client.update_release(release_object[:id], { status: :pending })
+    source = create_source(DEPLOY_PATH)
 
-#     source = create_source(DEPLOY_PATH)
+    config = Hash.strhash(JSON.parse(File.open(File.expand_path('./.platform')).read))[:release][:app]
 
-#     config = Hash.strhash(JSON.parse(File.open(File.expand_path('./.platform')).read))[:release][:app]
+    log 'turning on maintenance mode'
+    heroku_client.app.update(config[:id], { maintenance: true })
 
-#     log 'turning on maintenance mode'
-#     heroku_client.app.update(config[:id], { maintenance: true })
+    log 'creating the build'
+    build = Hash.strhash(heroku_client.build.create(config[:id], {
+      source_blob: {
+        url: source[:source_blob][:get_url]
+      }
+    }))
 
-#     log 'creating the build'
-#     build = Hash.strhash(heroku_client.build.create(config[:id], {
-#       source_blob: {
-#         url: source[:source_blob][:get_url]
-#       }
-#     }))
+    while true
+      sleep 1
+      info = Hash.strhash(heroku_client.build.info(config[:id], build[:id]))
+      log "build status: #{info[:status]}"
+      break if info[:status] != 'pending'
+    end
+    log 'finished the build'
 
-#     while true
-#       sleep 1
-#       info = Hash.strhash(heroku_client.build.info(config[:id], build[:id]))
-#       log "build status: #{info[:status]}"
-#       break if info[:status] != 'pending'
-#     end
-#     log 'finished the build'
+    log 'running db:migrate'
+    dyno = Hash.strhash(heroku_client.dyno.create(config[:id], {
+      command: 'bundle exec rake db:migrate'
+    }))
 
-#     log 'running db:migrate'
-#     dyno = Hash.strhash(heroku_client.dyno.create(config[:id], {
-#       command: 'bundle exec rake db:migrate'
-#     }))
+    while true
+      sleep 1
+      begin
+        Hash.strhash(heroku_client.dyno.info(config[:id], dyno[:id]))
+      rescue
+        log 'finished db:migrate'
+        break
+      end
+    end
 
-#     while true
-#       sleep 1
-#       begin
-#         Hash.strhash(heroku_client.dyno.info(config[:id], dyno[:id]))
-#       rescue
-#         log 'finished db:migrate'
-#         break
-#       end
-#     end
+    log 'restarting the app'
+    heroku_client.dyno.restart_all(config[:id])
 
-#     log 'restarting the app'
-#     heroku_client.dyno.restart_all(config[:id])
+    log 'turning off maintenance mode'
+    heroku_client.app.update(config[:id], { maintenance: false })
+    platform_client.update_release(release_object[:id], { status: :done })
+    log 'DONE'
+    terminate
+  end
 
-#     log 'turning off maintenance mode'
-#     heroku_client.app.update(config[:id], { maintenance: false })
-#     platform_client.update_release(release_object[:id], { status: :done })
-#     log 'DONE'
-#     terminate
-#   end
+  private
 
-#   private
+  def shutdown
+  end
 
-#   def shutdown
-#   end
+  def setup?
+    config = Hash.strhash(JSON.parse(File.open(File.expand_path('./.platform')).read))
+    config.key?('release')
+  end
 
-#   def setup?
-#     config = Hash.strhash(JSON.parse(File.open(File.expand_path('./.platform')).read))
-#     config.key?('release')
-#   end
+  def create_source(path)
+    log 'making the build'
 
-#   def create_source(path)
-#     log 'making the build'
+    `cd #{path} && tar -czf build.tgz ./* ./.platform`
 
-#     `cd #{path} && tar -czf build.tgz ./* ./.platform`
+    filepath = File.join(path, 'build.tgz')
 
-#     filepath = File.join(path, 'build.tgz')
+    log 'creating the source'
+    config = Hash.strhash(JSON.parse(File.open(File.expand_path('./.platform')).read))[:release][:app]
+    #TODO do this via httparty since not possible through platform-api
+    source = Hash.strhash(JSON.parse(`curl -n -X POST https://api.heroku.com/apps/#{config[:id]}/sources -H 'Accept: application/vnd.heroku+json; version=3'`))
 
-#     log 'creating the source'
-#     config = Hash.strhash(JSON.parse(File.open(File.expand_path('./.platform')).read))[:release][:app]
-#     #TODO do this via httparty since not possible through platform-api
-#     source = Hash.strhash(JSON.parse(`curl -n -X POST https://api.heroku.com/apps/#{config[:id]}/sources -H 'Accept: application/vnd.heroku+json; version=3'`))
+    log 'uploading the source'
+    # perform the upload
+    #TODO do this via httparty since not possible through platform-api
+    `curl \"#{source[:source_blob][:put_url]}\" -X PUT -H 'Content-Type:' --data-binary @#{filepath}`
 
-#     log 'uploading the source'
-#     # perform the upload
-#     #TODO do this via httparty since not possible through platform-api
-#     `curl \"#{source[:source_blob][:put_url]}\" -X PUT -H 'Content-Type:' --data-binary @#{filepath}`
+    log 'removing the local build'
+    FileUtils.rm(filepath)
 
-#     log 'removing the local build'
-#     FileUtils.rm(filepath)
+    source
+  end
 
-#     source
-#   end
-
-#   def log(message)
-#     Pakyow.logger.debug "[Release #{@id}] #{message}"
-#   end
-# end
+  def log(message)
+    Pakyow.logger.debug "[Release #{@id}] #{message}"
+  end
+end
--- lib/app/misc/websocket_client.rb
+++ lib/app/misc/websocket_client.rb
@@ -1,83 +1,68 @@
-# TODO: refactor away from celluloid
-# require 'bundler/setup'
-# require 'websocket-client-simple'
-# require 'httparty'
-# require 'json'
-# require 'securerandom'
+require 'websocket-client-simple'
+require 'httparty'
+require 'json'
+require 'securerandom'
+require 'concurrent'
 
-# class WebSocketPing
-#   include Celluloid
+class WebSocketClient
+  def initialize(context, platform_client, platform_info)
+    @platform_client = platform_client
+    @platform_info = platform_info
 
-#   attr_reader :ws
+    if ws = socket
+      ws.on :message do |msg|
+        msg = Hash.strhash(JSON.parse(msg.data.to_s))
 
-#   def initialize(ws)
-#     @ws = ws
+        if payload = msg[:payload]
+          if event = payload[:event]
+            context.data(:app_event).mutated(event)
+          end
 
-#     async.run
-#   end
+          if collaborator = payload[:collaborator]
+            context.data(:collaborator).mutated(collaborator)
+          end
 
-#   def run
-#     loop do
-#       sleep 15
-#       ws.send({ action: 'ping' }.to_json)
-#     end
-#   end
-# end
+          if release = payload[:release]
+            context.data(:release).mutated(release)
+          end
+        end
+      end
 
-# class WebSocketClient
-#   def initialize(context, platform_client, platform_info)
-#     @platform_client = platform_client
-#     @platform_info = platform_info
+      ws.on :open do
+        puts '!!! opened'
+      end
 
-#     if ws = socket
-#       ws.on :message do |msg|
-#         msg = Hash.strhash(JSON.parse(msg.data.to_s))
+      ws.on :close do |e|
+        puts '!!! closed'
+        # exit 1
+      end
 
-#         if payload = msg[:payload]
-#           if event = payload[:event]
-#             context.data(:app_event).mutated(event)
-#           end
+      ws.on :error do |e|
+        puts '!!! error'
+        puts e.message
+        puts e.backtrace
+      end
 
-#           if collaborator = payload[:collaborator]
-#             context.data(:collaborator).mutated(collaborator)
-#           end
+      Concurrent::Future.execute do
+        loop do
+          sleep 15
+          ws.send({ action: 'ping' }.to_json)
+        end
+      end
+    end
+  end
 
-#           if release = payload[:release]
-#             context.data(:release).mutated(release)
-#           end
-#         end
-#       end
+  private
 
-#       ws.on :open do
-#         puts '!!! opened'
-#       end
+  def shutdown
+    puts '!!! shutting down'
+    socket.close
+  end
 
-#       ws.on :close do |e|
-#         puts '!!! closed'
-#         # exit 1
-#       end
-
-#       ws.on :error do |e|
-#         puts '!!! error'
-#         puts e.message
-#         puts e.backtrace
-#       end
-
-#       WebSocketPing.new(ws)
-#     end
-#   end
-
-#   private
-
-#   def shutdown
-#     puts '!!! shutting down'
-#     socket.close
-#   end
-
-#   def socket
-#     return if @platform_info.empty?
-#     return @socket if @socket
-#     info = @platform_client.socket
-#     @socket = WebSocket::Client::Simple.connect "ws://#{$platform_uri}?socket_connection_id=#{info[:socket_connection_id]}&socket_key=#{info[:socket_key]}"
-#   end
-# end
+  def socket
+    return if @platform_info.empty?
+    return @socket if @socket
+    info = @platform_client.socket
+    @socket = WebSocket::Client::Simple.connect "ws://#{$platform_uri}?socket_connection_id=#{info[:socket_connection_id]}&socket_key=#{info[:socket_key]}"
+  end
+end
