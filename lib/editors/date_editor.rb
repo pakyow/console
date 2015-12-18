929f250d7ea97a459a7b69463e787a6b9855c212
Bryan Powell <bryan@metabahn.com>
2015-12-18 16:22:03 -0600

    Date input component

--- lib/editors/date_editor.rb
+++ lib/editors/date_editor.rb
@@ -1,3 +1,3 @@
 Pakyow::Console.editor :date, :float do
-  Pakyow::Presenter::View.new('<input type="date" data-scope="editor" style="width: 400px">')
+  Pakyow::Presenter::View.new('<input type="text" data-scope="editor" style="width: 400px" placeholder="yyyy-mm-dd" data-ui="date">')
 end
