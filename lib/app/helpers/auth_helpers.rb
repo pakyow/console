1a641c9a51472fc6a8ba1947605b66b967703fcc
Bryan Powell <bryan@metabahn.com>
2015-12-21 10:49:23 -0600

    Abstract auth helpers into its own module

--- lib/app/helpers/auth_helpers.rb
+++ lib/app/helpers/auth_helpers.rb
@@ -63,40 +63,13 @@ module Pakyow::Helpers
     end
   end
 
-  def platform_client(email = nil, token = nil)
-    if email && token
-      PlatformClient.new(email, token, platform_info)
-    elsif Pakyow.app.env == :development && !platform_creds.empty?
-      PlatformClient.new(platform_creds[:email], platform_creds[:token], platform_info)
-    elsif console_authed?
-      PlatformClient.new(session[:platform_email], session[:platform_token], platform_info)
-    end
-  end
-
-  def platform_creds
-    return {} unless File.exists?(pakyow_platform_file_path)
-    Hash.strhash(JSON.parse(File.open(pakyow_platform_file_path).read))
-  end
-
   def platform_info
     return {} unless File.exists?(platform_file_path)
     Hash.strhash(JSON.parse(File.open(platform_file_path).read))
   end
 
-  def pakyow_platform_file_path
-    File.expand_path('~/.pakyow')
-  end
-
   def platform_file_path
     File.expand_path('./.platform')
-  end
-
-  def setup_platform_socket(auth_info = nil)
-    return if $socket
-    return if (auth_info.nil? || auth_info.empty?) && !platform_authed?
-    return if $platform_uri.nil?
-    auth_info ||= { email: session[:platform_email], token: session[:platform_token] }
-    $socket = WebSocketClient.new(self, platform_client(auth_info[:email], auth_info[:token]), auth_info)
   end
 
   def reconnect_platform_socket(auth_info = nil)
---
+++ lib/app/helpers/platform_helpers.rb
@@ -0,0 +1,44 @@
+module Pakyow
+  module Helpers
+    module Platform
+      def platform_client(email = nil, token = nil)
+        if email && token
+          PlatformClient.new(email, token, platform_info)
+        elsif Pakyow::Config.env == :development && !platform_creds.empty?
+          PlatformClient.new(platform_creds[:email], platform_creds[:token], platform_info)
+        elsif console_authed?
+          PlatformClient.new(session[:platform_email], session[:platform_token], platform_info)
+        end
+      end
+
+      def platform_creds
+        return {} unless File.exists?(pakyow_platform_file_path)
+        Hash.strhash(JSON.parse(File.open(pakyow_platform_file_path).read))
+      end
+
+      def pakyow_platform_file_path
+        File.expand_path('~/.pakyow')
+      end
+
+      def setup_platform_socket(auth_info = nil)
+        return if $socket
+        return if (auth_info.nil? || auth_info.empty?) && !platform_authed?
+        return if $platform_uri.nil?
+        auth_info ||= { email: session[:platform_email], token: session[:platform_token] }
+        $socket = WebSocketClient.new(self, platform_client(auth_info[:email], auth_info[:token]), auth_info)
+      end
+    end
+  end
+end
+
+class Pakyow::App
+  include Pakyow::Helpers::Platform
+end
+
+class Pakyow::CallContext
+  include Pakyow::Helpers::Platform
+end
+
+class Pakyow::UI::Mutable
+  include Pakyow::Helpers::Platform
+end
