e8cc08bb6702630a6308d2cc0e21deb9e84dfc12
Ryan Clardy <ryan@metabahn.com>
2016-03-22 16:47:29 -0500

    Fix float editor

--- lib/pakyow/console/editors/date_editor.rb
+++ lib/pakyow/console/editors/date_editor.rb
@@ -1,3 +1,3 @@
-Pakyow::Console.editor :date, :float do
+Pakyow::Console.editor :date do
   Pakyow::Presenter::View.new('<input type="text" data-scope="editor" style="width: 400px" placeholder="yyyy-mm-dd" data-ui="date">')
 end
