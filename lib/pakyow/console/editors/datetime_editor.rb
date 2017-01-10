9671fb9ca9806ac5aba89957450a618358ccbefc
Bryan Powell <bryan@metabahn.com>
2017-01-10 08:23:16 -0800

    Fix datepicker

--- lib/pakyow/console/editors/datetime_editor.rb
+++ lib/pakyow/console/editors/datetime_editor.rb
@@ -1,8 +1,5 @@
 Pakyow::Console.editor :datetime do
   Pakyow::Presenter::View.new(
-    '<link href="/console/flatpickr/flatpickr.css" rel="stylesheet" type="text/css">' \
-    '<script src="/console/flatpickr/flatpickr.js"></script>' \
-    '<script src="/console/scripts/components/flatpickr.js"></script>' \
     '<input type="text" data-scope="editor" class="field-datetime" data-ui="flatpickr">'
   )
 end
